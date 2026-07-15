9e401efd7a6bad9622d39710eab52ecded6780bf
mikinol <sm44aksdmiki13877kfh@gmail.com>
2026-07-15 19:09:48 +0300

    Final подфикс

--- default.nix
+++ default.nix
@@ -29,7 +29,7 @@ stdenv.mkDerivation {
     substituteInPlace build_stage/bin/update_resourcepack \
         --replace-fail "#!/bin/sh" "#!${dash}/bin/dash" \
         --replace-fail "sha1sum" "${coreutils-full}/bin/sha1sum" \
-        --replace-fail "./publish_resourcepack" "$out/bin/publish-resourcepack" \
+        --replace-fail "./publish_resourcepack" "$out/bin/publish_resourcepack" \
         --replace-fail "grep" "${gnugrep}/bin/grep" \
         --replace-fail "sed" "${gnused}/bin/sed"
 
@@ -47,7 +47,7 @@ stdenv.mkDerivation {
 
   doCheck = true;
   checkPhase = ''
-    shellcheck -s dash build_stage/bin/update_resourcepack build_stage/internal/publish-resourcepack
+    shellcheck -s dash build_stage/bin/update_resourcepack build_stage/internal/publish_resourcepack
   '';
 
   installPhase = ''
