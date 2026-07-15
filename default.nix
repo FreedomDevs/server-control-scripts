73a1717a271268c9961db0e2dbb87f72bba4f711
mikinol <sm44aksdmiki13877kfh@gmail.com>
2026-07-15 17:16:44 +0300

    Сделал default.nix для сборки

---
+++ default.nix
@@ -0,0 +1,58 @@
+{
+  stdenv,
+  shellcheck,
+  dash,
+  # Программы
+  coreutils-full,
+  util-linux,
+  gnugrep,
+  gnused,
+  # Переменные
+  outpath ? /var/www/resurcepacks/,
+  resourcepack_namespace_path ? /run/agenix/resourcepack_namespace,
+}:
+stdenv.mkDerivation {
+  pname = "elysium-server-control-scripts";
+  version = "1.0";
+
+  src = ./.;
+
+  dontUnpack = false;
+
+  nativeBuildInputs = [shellcheck];
+  buildInputs = [dash];
+
+  buildPhase = ''
+    mkdir -p build_stage/bin build_stage/internal
+
+    cp update_resourcepack build_stage/bin/
+    substituteInPlace build_stage/bin/update_resourcepack \
+        --replace-fail "#!/bin/sh" "#!${dash}/bin/dash" \
+        --replace-fail "sha1sum" "${coreutils-full}/bin/sha1sum" \
+        --replace-fail "./publish_resourcepack" "$out/bin/publish-resourcepack" \
+        --replace-fail "grep" "${gnugrep}/bin/grep" \
+        --replace-fail "sed" "${gnused}/bin/sed"
+
+    cp publish_resourcepack build_stage/internal/
+    substituteInPlace build_stage/internal/publish_resourcepack \
+        --replace-fail "#!/bin/sh" "#!${dash}/bin/dash" \
+        --replace-fail "out/" "${outpath}" \
+        --replace-fail "cat" "${coreutils-full}/cat" \
+        --replace-fail "resourcepack_namespace" "${resourcepack_namespace_path}" \
+        --replace-fail "uuidgen" "${util-linux}/bin/uuidgen" \
+        --replace-fail "head" "${coreutils-full}/bin/head" \
+        --replace-fail "rm" "${coreutils-full}/bin/rm" \
+        --replace-fail "mv" "${coreutils-full}/bin/mv"
+  '';
+
+  doCheck = true;
+  checkPhase = ''
+    shellcheck -s dash build_stage/bin/update_resourcepack build_stage/internal/publish-resourcepack
+  '';
+
+  installPhase = ''
+    mkdir -p $out/bin $out/internal
+    cp -r build_stage/bin/ $out/
+    cp -r build_stage/internal/ $out/
+  '';
+}
